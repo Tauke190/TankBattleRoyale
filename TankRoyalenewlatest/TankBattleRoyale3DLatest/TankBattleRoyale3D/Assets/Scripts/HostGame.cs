2fa640b0b064f489863457e45807f7264e92b56e
Utsavthegreat <utsavideal@gmail.com>
2018-10-31 20:02:39 +0545

    Hosting

---
+++ TankRoyalenewlatest/TankBattleRoyale3DLatest/TankBattleRoyale3D/Assets/Scripts/HostGame.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class HostGame : MonoBehaviour {
+
+	[SerializeField]
+	uint roomSize = 6;
+	string roomName;
+	public NetworkManager networkManager;
+
+	void Start(){
+		networkManager = NetworkManager.singleton;
+		if (networkManager.matchMaker == null)
+			networkManager.StartMatchMaker ();
+	}
+
+	public void SetRoomName(string name){
+		roomName = name;
+	}
+
+	public void CreateRoom(){
+		if(roomName != "" && roomName != null){
+			Debug.Log ("Creating Room: " + roomName);
+			networkManager.matchMaker.CreateMatch (roomName, roomSize, true, "", "", "", 0, 0, networkManager.OnMatchCreate);
+		}
+	}
+}
